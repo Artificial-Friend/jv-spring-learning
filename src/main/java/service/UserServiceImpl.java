c82905a1ecc8a923acc3b8c08ff3515dc6213005
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-02-09 13:11:57 +0200

    checkstyle

--- src/main/java/service/UserServiceImpl.java
+++ src/main/java/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package service;
 import dao.UserDao;
 import java.util.List;
 import model.User;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
