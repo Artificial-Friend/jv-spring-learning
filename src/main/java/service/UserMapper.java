762b6d03df909e3e517acef707b41a4180da2e56
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2021-02-10 18:14:07 +0200

    p3c4 I don't like it.

--- src/main/java/service/UserMapper.java
+++ src/main/java/service/UserMapper.java
@@ -1,15 +1,8 @@
 package service;
 
 import dto.UserResponseDto;
-import java.util.List;
 import model.User;
 
 public interface UserMapper {
-    List<User> getUserListFromDto(List<UserResponseDto> userResponseDto);
-
-    User getUserFromDto(UserResponseDto userResponseDto);
-
-    List<UserResponseDto> getUserDtoListFromUser(List<User> users);
-
-    UserResponseDto getUserDtoFromUser(User user);
+    UserResponseDto toDto(User user);
 }
